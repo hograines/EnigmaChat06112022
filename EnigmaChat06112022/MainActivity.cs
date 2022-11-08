8ddebfbf708e66480990851189dfcf79d2fdf962
StudentC <StudentC@CYBERN-10>
2022-11-08 14:36:22 +0200

    Add project files.

---
+++ EnigmaChat06112022/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Widget;
+
+namespace EnigmaChat06112022
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btn;
+        Dialog d;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            btn = FindViewById<Button>(Resource.Id.btn);
+
+            d = new Dialog(this);
+            d.SetContentView(Resource.Layout.layout1);
+            d.SetCancelable(true);
+
+
+            btn.Click += Btn_Click;
+        }
+
+        private void Btn_Click(object sender, System.EventArgs e)
+        {
+            d.Show();
+        }
+    }
+}
